c7746de80fa07c51a79c6de04d3b8c162d7add77
Bradyn <Jenkins22051@lcsd2kids.org>
2021-05-06 10:59:13 -0600

    Java and CSS is working now i need to update this when i get home

--- crazy-script.js
+++ crazy-script.js
@@ -1,30 +1,12 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
-
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+const Container = document.querySelector("#Container");
+let numOfTimes = 256;
+for (let i = 0;i <numOfTimes;i++) {
+    const singleCell = document.createElement ("div");
+    singleCell.textContent = "Hello";
+    singleCell.style.color = "Green";
+    singleCell.classList.add("bg-red")
+    singleCell.addEventListener('click', () => {
+        alert ("Hello World");
+    });
+    Container.appendChild(singleCell);
 }
-
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
